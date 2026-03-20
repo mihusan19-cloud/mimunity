50e6ef084b3518b4fa00596af74f58afd40bb3c3
mihusan19-cloud <mihusan19@gmail.com>
2026-03-20 11:06:54 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,22 @@
-self.addEventListener('fetch', function(event) {
-   保持空白即可，這只是為了符合 PWA 安裝門檻
+const CACHE_NAME = 'mimmunity-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  // 這裡放入你需要快取的其他路徑
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
