18317db922e7c4589ebd2d3f8fefb4d5a823ba17
mihusan19-cloud <mihusan19@gmail.com>
2026-07-03 15:52:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,60 @@
-const CACHE_NAME = 'mimmunity-v1.9';
+const CACHE_NAME = 'mimunity-v3';
 const ASSETS = [
   './',
   './index.html',
-  // 這裡放入你需要快取的其他路徑
+  './manifest.json',
+  './css/style.css',
+  './js/config.js',
+  './js/utils.js',
+  './js/notification.js',
+  './js/app.js',
+  './js/auth.js',
+  './js/profile.js',
+  './js/chat.js',
+  './js/notebook.js',
+  './js/avatarfx.js',
+  './icon-192.png',
+  './icon-512.png',
+  './effects/fx1.svg',
+  './effects/fx2.svg',
+  './effects/fx3.svg',
+  './effects/fx4.svg',
+  './effects/fx5.svg',
+  './effects/fx6.svg',
+  './effects/effectsfx7.png'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys
+        .filter((key) => key !== CACHE_NAME)
+        .map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) return;
+
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
